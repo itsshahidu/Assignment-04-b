c03583d1fe82056da661510ea109c47a187617c0
Shahid Ullah <142977934+itsshahidu@users.noreply.github.com>
2024-01-16 17:33:30 +0500

    Add files via upload

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@email.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'shahid',
+    email: 'shahid@email.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@email.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+];
+
+export default users;
